77ab06a63660f9c93cdad244f228d202d92a51d9
minosletitgo <380122248@qq.com>
2024-09-15 23:16:34 +0800

    1

--- contracts/Hello.sol
+++ contracts/Hello.sol
@@ -8,4 +8,21 @@ contract Hello {
         emit DoLog("Hello, World!"); 
         return "Hello, World!";
     }
+
+    function print0() external returns (string memory) {
+        emit DoLog("Hello, World!"); 
+        return "Hello, World!";
+    }
+
+    function print1() external returns (string memory) {
+        string memory str = unicode"print1() = 你好，世界";
+        emit DoLog(str);
+        return str;
+    }
+
+    function print2() external returns (string memory) {
+        string memory str = unicode"print2() = nihao，shijie";
+        emit DoLog(str);
+        return str;
+    }    
 }
--- scripts/deploy_Hello.js
+++ scripts/deploy_Hello.js
@@ -7,13 +7,18 @@ async function main() {
     await hello.deployed();
     console.log("hello.address = ", hello.address); 
 
-    // 监听 ContractDeployed 事件 
+    // 监听 事件 
     hello.on("DoLog", (str) => {
         console.log(`hello event detected!`);
         console.log(`str: ${str}`);
     });    
 
     console.log(" => ", await hello.greet()); 
+    console.log(" => ", await hello.print0()); 
+    console.log(" => ", await hello.print1()); 
+    console.log(" => ", await hello.print2()); 
+
+    console.log(" Over "); 
 }
 
 main().catch((error) => {
