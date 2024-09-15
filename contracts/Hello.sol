de7e3b039851eda07b93ebe72b1e13ebe148bff1
minosletitgo <380122248@qq.com>
2024-09-15 20:09:52 +0800

    Update Hello.sol

--- contracts/Hello.sol
+++ contracts/Hello.sol
@@ -5,7 +5,7 @@ contract Hello {
     event DoLog(string str);
     // 定义一个返回字符串的公共函数
     function greet() public returns (string memory) {
-        emit DoLog("Hello, World!");
+        emit DoLog("Hello, World!"); 
         return "Hello, World!";
     }
 }
